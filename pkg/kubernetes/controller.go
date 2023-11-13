bfbba81baec29ea39096921a2b2740930bc28424
ohbyeongmin <fbottlem@gmail.com>
2023-11-13 07:16:26 +0000

    feature: getResourcePath, getYAMLfiles

--- pkg/kubernetes/controller.go
+++ pkg/kubernetes/controller.go
@@ -10,3 +10,7 @@ func (k *Kubernetes) GetPodStatus(namespace, name string) (string, error) {
 	pod, err := k.client.CoreV1().Pods(namespace).Get(context.TODO(), name, v1.GetOptions{})
 	return string(pod.Status.Phase), err
 }
+
+func (k *Kubernetes) ApplyResources() {
+
+}
---
+++ pkg/kubernetes/utils_test.go
@@ -0,0 +1,60 @@
+package kubernetes
+
+import (
+	"os"
+	"path"
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/util/yaml"
+)
+
+type UtilsTestSuite struct {
+	suite.Suite
+	deploymentsPath string
+}
+
+func (uts *UtilsTestSuite) SetupTest() {
+	cPath, _ := os.Getwd()
+	uts.deploymentsPath = path.Join(cPath, "../../deployments")
+}
+
+func (uts *UtilsTestSuite) TestGetResourcesPath() {
+	tests := []struct {
+		name string
+	}{
+		{"deployer"},
+		{"l2geth"},
+	}
+
+	for _, tt := range tests {
+		p := getResourcesPath(tt.name)
+		uts.Equal(p, uts.deploymentsPath+"/"+tt.name, "it is should be equal")
+	}
+}
+
+func (uts *UtilsTestSuite) TestGetYAMLfiles() {
+	tests := []struct {
+		name      string
+		resources []string
+	}{
+		{"l2geth", []string{"Service", "StatefulSet"}},
+	}
+
+	for _, tt := range tests {
+		for i, yamlfile := range getYAMLfiles(tt.name) {
+			jsonBytes, _ := yaml.ToJSON(yamlfile)
+			object, _ := runtime.Decode(unstructured.UnstructuredJSONScheme, jsonBytes)
+			uObject, _ := object.(*unstructured.Unstructured)
+
+			uts.Equal(tt.resources[i], uObject.GetKind())
+		}
+	}
+}
+
+func TestRunSuite(t *testing.T) {
+	suite.Run(t, new(UtilsTestSuite))
+}
