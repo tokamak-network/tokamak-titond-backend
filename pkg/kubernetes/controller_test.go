b5bdb0b8c54fedae21da24a1570cbc15847222b5
ohbyeongmin <fbottlem@gmail.com>
2023-11-20 06:44:06 +0000

    mod test controller

--- pkg/kubernetes/controller_test.go
+++ pkg/kubernetes/controller_test.go
@@ -12,20 +12,11 @@ func TestCreate(t *testing.T) {
 	fakeKubernetes := &Kubernetes{fakeClient}
 
 	t.Run("Create StatefulSet", func(t *testing.T) {
-		t.Run("Not StatefulSet yaml file", func(t *testing.T) {
-			file := GetYAMLfile("l2geth", "service")
-			obj := ConvertBytestoObject(file)
-
-			_, err := fakeKubernetes.CreateStatefulSet("test", obj)
-
-			assert.EqualError(t, err, "This is not StatefulSet")
-		})
-
 		t.Run("Create StatefulSet Successfully", func(t *testing.T) {
-			file := GetYAMLfile("l2geth", "statefulset")
-			obj := ConvertBytestoObject(file)
+			obj := GetObject("l2geth", "statefulset")
+			sfs, _ := ConvertToStatefulSet(obj)
 
-			res, err := fakeKubernetes.CreateStatefulSet("test", obj)
+			res, err := fakeKubernetes.CreateStatefulSet("test", sfs)
 
 			assert.NoError(t, err, "must be not error")
 			assert.Equal(t, "l2geth", res.GetName(), "must be l2geth")
@@ -33,20 +24,11 @@ func TestCreate(t *testing.T) {
 	})
 
 	t.Run("Create Service", func(t *testing.T) {
-		t.Run("Not Service yaml file", func(t *testing.T) {
-			file := GetYAMLfile("l2geth", "statefulset")
-			obj := ConvertBytestoObject(file)
-
-			_, err := fakeKubernetes.CreateService("test", obj)
-
-			assert.EqualError(t, err, "This is not Service")
-		})
-
 		t.Run("Create Service Successfully", func(t *testing.T) {
-			file := GetYAMLfile("l2geth", "service")
-			obj := ConvertBytestoObject(file)
+			obj := GetObject("l2geth", "service")
+			svc, _ := ConvertToService(obj)
 
-			res, err := fakeKubernetes.CreateService("test", obj)
+			res, err := fakeKubernetes.CreateService("test", svc)
 
 			assert.NoError(t, err, "must be not error")
 			assert.Equal(t, "l2geth-svc", res.GetName(), "must be l2geth-svc")
@@ -54,20 +36,11 @@ func TestCreate(t *testing.T) {
 	})
 
 	t.Run("Create PersistentVolumeClaim", func(t *testing.T) {
-		t.Run("Not PersistentVolumeClaim yaml file", func(t *testing.T) {
-			file := GetYAMLfile("l2geth", "statefulset")
-			obj := ConvertBytestoObject(file)
-
-			_, err := fakeKubernetes.CreatePersistentVolumeClaim("test", obj)
-
-			assert.EqualError(t, err, "This is not PersistentVolumeClaim")
-		})
-
 		t.Run("Create PersistentVolumeClaim Successfully", func(t *testing.T) {
-			file := GetYAMLfile("l2geth", "pvc")
-			obj := ConvertBytestoObject(file)
+			obj := GetObject("l2geth", "pvc")
+			pvc, _ := ConvertToPersistentVolumeClaim(obj)
 
-			res, err := fakeKubernetes.CreatePersistentVolumeClaim("test", obj)
+			res, err := fakeKubernetes.CreatePersistentVolumeClaim("test", pvc)
 
 			assert.NoError(t, err, "must be not error")
 			assert.Equal(t, "l2geth-pvc", res.GetName(), "must be l2geth-pvc")
@@ -75,20 +48,11 @@ func TestCreate(t *testing.T) {
 	})
 
 	t.Run("Create ConfilgMap", func(t *testing.T) {
-		t.Run("Not ConfilgMap yaml file", func(t *testing.T) {
-			file := GetYAMLfile("l2geth", "statefulset")
-			obj := ConvertBytestoObject(file)
-
-			_, err := fakeKubernetes.CreateConfigMap("test", obj)
-
-			assert.EqualError(t, err, "This is not ConfilgMap")
-		})
-
 		t.Run("Create ConfilgMap Successfully", func(t *testing.T) {
-			file := GetYAMLfile("l2geth", "configMap")
-			obj := ConvertBytestoObject(file)
+			obj := GetObject("l2geth", "configMap")
+			configMap, _ := ConvertToConfigMap(obj)
 
-			res, err := fakeKubernetes.CreateConfigMap("test", obj)
+			res, err := fakeKubernetes.CreateConfigMap("test", configMap)
 
 			assert.NoError(t, err, "must be not error")
 			assert.Equal(t, "l2geth", res.GetName(), "must be l2geth")
@@ -96,20 +60,11 @@ func TestCreate(t *testing.T) {
 	})
 
 	t.Run("Create Ingress", func(t *testing.T) {
-		t.Run("Not Ingress yaml file", func(t *testing.T) {
-			file := GetYAMLfile("l2geth", "statefulset")
-			obj := ConvertBytestoObject(file)
-
-			_, err := fakeKubernetes.CreateIngress("test", obj)
-
-			assert.EqualError(t, err, "This is not Ingress")
-		})
-
 		t.Run("Create Ingress Successfully", func(t *testing.T) {
-			file := GetYAMLfile("l2geth", "ingress")
-			obj := ConvertBytestoObject(file)
+			obj := GetObject("l2geth", "ingress")
+			ingress, _ := ConvertToIngress(obj)
 
-			res, err := fakeKubernetes.CreateIngress("test", obj)
+			res, err := fakeKubernetes.CreateIngress("test", ingress)
 
 			assert.NoError(t, err, "must be not error")
 			assert.Equal(t, "ingress-l2geth", res.GetName(), "must be ingress-l2geth")
