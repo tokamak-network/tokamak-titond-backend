913fc3f3348648c53d80888ba695c9929e9884c7
ohbyeongmin <fbottlem@gmail.com>
2023-11-23 15:35:47 +0900

    error print

--- pkg/api/l2geth.go
+++ pkg/api/l2geth.go
@@ -11,7 +11,7 @@ type l2gethConfig struct {
 	data map[string]string
 }
 
-func (t *TitondAPI) CreateL2Geth(data *model.Component /*TODO : get params for config, namespace*/) (*model.Component, error) {
+func (t *TitondAPI) CreateL2Geth(data *model.Component /*TODO : get params for config, namespace*/) *model.Component {
 	// TODO : deal with DB
 	// t.db.CreateComponent()
 
@@ -27,29 +27,22 @@ func (t *TitondAPI) CreateL2Geth(data *model.Component /*TODO : get params for c
 	config.data["ETH1_CONFIRMATION_DEPTH"] = "1"
 	config.data["GASPRICE"] = "100"
 
-	l2GethChan := make(chan error)
+	go t.createL2Geth(namespace, config)
 
-	go t.createL2Geth(namespace, config, l2GethChan)
-
-	if err := <-l2GethChan; err != nil {
-		return nil, err
-	}
-	close(l2GethChan)
-
-	return data, nil
+	return data
 }
 
-func (t *TitondAPI) createL2Geth(namespace string, config *l2gethConfig, res chan error) {
+func (t *TitondAPI) createL2Geth(namespace string, config *l2gethConfig) {
 	obj := kubernetes.GetObject("l2geth", "configMap")
 	cm, err := kubernetes.ConvertToConfigMap(obj)
 	if err != nil {
-		res <- err
+		fmt.Printf("createL2Geth error: %s\n", err)
 		return
 	}
 
 	createdConfigMap, err := t.k8s.CreateConfigMapWithConfig(namespace, cm, config.data)
 	if err != nil {
-		res <- err
+		fmt.Printf("createL2Geth error: %s\n", err)
 		return
 	}
 	fmt.Printf("Created L2Geth ConfigMap: %s\n", createdConfigMap.GetName())
@@ -57,13 +50,13 @@ func (t *TitondAPI) createL2Geth(namespace string, config *l2gethConfig, res cha
 	obj = kubernetes.GetObject("l2geth", "pvc")
 	pvc, err := kubernetes.ConvertToPersistentVolumeClaim(obj)
 	if err != nil {
-		res <- err
+		fmt.Printf("createL2Geth error: %s\n", err)
 		return
 	}
 
 	createdPVC, err := t.k8s.CreatePersistentVolumeClaim(namespace, pvc)
 	if err != nil {
-		res <- err
+		fmt.Printf("createL2Geth error: %s\n", err)
 		return
 	}
 	fmt.Printf("Created L2Geth PersistentVolumeClaim: %s\n", createdPVC.GetName())
@@ -71,13 +64,13 @@ func (t *TitondAPI) createL2Geth(namespace string, config *l2gethConfig, res cha
 	obj = kubernetes.GetObject("l2geth", "service")
 	svc, err := kubernetes.ConvertToService(obj)
 	if err != nil {
-		res <- err
+		fmt.Printf("createL2Geth error: %s\n", err)
 		return
 	}
 
 	createdSVC, err := t.k8s.CreateService(namespace, svc)
 	if err != nil {
-		res <- err
+		fmt.Printf("createL2Geth error: %s\n", err)
 		return
 	}
 	fmt.Printf("Created L2Geth Service: %s\n", createdSVC.GetName())
@@ -85,13 +78,13 @@ func (t *TitondAPI) createL2Geth(namespace string, config *l2gethConfig, res cha
 	obj = kubernetes.GetObject("l2geth", "statefulset")
 	sfs, err := kubernetes.ConvertToStatefulSet(obj)
 	if err != nil {
-		res <- err
+		fmt.Printf("createL2Geth error: %s\n", err)
 		return
 	}
 
 	createdSFS, err := t.k8s.CreateStatefulSet(namespace, sfs)
 	if err != nil {
-		res <- err
+		fmt.Printf("createL2Geth error: %s\n", err)
 		return
 	}
 	fmt.Printf("Created L2Geth StatefulSet: %s\n", createdSFS.GetName())
@@ -99,15 +92,14 @@ func (t *TitondAPI) createL2Geth(namespace string, config *l2gethConfig, res cha
 	obj = kubernetes.GetObject("l2geth", "ingress")
 	ingress, err := kubernetes.ConvertToIngress(obj)
 	if err != nil {
-		res <- err
+		fmt.Printf("createL2Geth error: %s\n", err)
 		return
 	}
 
 	createdIngress, err := t.k8s.CreateIngress(namespace, ingress)
 	if err != nil {
-		res <- err
+		fmt.Printf("createL2Geth error: %s\n", err)
 		return
 	}
 	fmt.Printf("Created L2Geth Ingress: %s\n", createdIngress.GetName())
-	res <- nil
 }
