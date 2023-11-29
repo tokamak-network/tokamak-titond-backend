f6291a1d886d439da729795c0c6f83f18a150f1e
ohbyeongmin <fbottlem@gmail.com>
2023-11-29 07:15:58 +0000

    use generatenamespace func

--- pkg/api/component.go
+++ pkg/api/component.go
@@ -16,7 +16,7 @@ func (t *TitondAPI) CreateComponent(component *model.Component, config *Componen
 	var result *model.Component
 	var err error
 
-	config.Namespace = fmt.Sprintf("namespace-%d", component.NetworkID)
+	config.Namespace = generateNamespace(component.NetworkID)
 
 	switch component.Type {
 	case "l2geth":
