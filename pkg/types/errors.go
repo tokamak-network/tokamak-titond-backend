6ee34bcb536dc151eb824f7235e05f8e583e9360
ohbyeongmin <fbottlem@gmail.com>
2023-11-29 06:48:00 +0000

    Add error invalid component type

--- pkg/types/errors.go
+++ pkg/types/errors.go
@@ -3,7 +3,8 @@ package types
 import "errors"
 
 var (
-	ErrBadRequest       = errors.New("bad request")
-	ErrResourceNotFound = errors.New("resource not found")
-	ErrInternalServer   = errors.New("internal server error")
+	ErrBadRequest           = errors.New("bad request")
+	ErrResourceNotFound     = errors.New("resource not found")
+	ErrInternalServer       = errors.New("internal server error")
+	ErrInvalidComponentType = errors.New("invalid component type")
 )
