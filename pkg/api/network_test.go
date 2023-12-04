f934d1fa8d668977945558243b2314e2dd0aba0c
nguyenzung <nguyenvietdungcs52@gmail.com>
2023-12-04 17:47:26 +0700

    OR-1163 add mock k8s client

--- pkg/api/network_test.go
+++ pkg/api/network_test.go
@@ -4,6 +4,9 @@ import (
 	"testing"
 
 	"github.com/tokamak-network/tokamak-titond-backend/pkg/model"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	networkv1 "k8s.io/api/networking/v1"
 )
 
 type MockDBClient struct {
@@ -58,6 +61,102 @@ func (client *MockDBClient) UpdateComponent() {
 
 func (client *MockDBClient) DeleteComponent() {
 
+}
+
+type MockK8sClient struct {
+	err         error
+	podStatus   string
+	fileContent string
+	namespace   *corev1.Namespace
+	podList     *corev1.PodList
+	service     *corev1.Service
+	pvc         *corev1.PersistentVolumeClaim
+	configmap   *corev1.ConfigMap
+	ingress     *networkv1.Ingress
+	deployment  *appsv1.Deployment
+	statefulSet *appsv1.StatefulSet
+	stdout      []byte
+	stderr      []byte
+}
+
+func (client *MockK8sClient) GetPodStatus(namespace, name string) (string, error) {
+	return client.podStatus, client.err
+}
+
+func (client *MockK8sClient) CreateStatefulSet(namespace string, statefulSet *appsv1.StatefulSet) (*appsv1.StatefulSet, error) {
+	return client.statefulSet, client.err
+}
+
+func (client *MockK8sClient) CreateService(namespace string, service *corev1.Service) (*corev1.Service, error) {
+	return client.service, client.err
+}
+
+func (client *MockK8sClient) CreatePersistentVolumeClaim(namespace string, pvc *corev1.PersistentVolumeClaim) (*corev1.PersistentVolumeClaim, error) {
+	return client.pvc, client.err
+}
+
+func (client *MockK8sClient) CreateConfigMapWithConfig(namespace string, configMap *corev1.ConfigMap, config map[string]string) (*corev1.ConfigMap, error) {
+	return client.configmap, client.err
+}
+
+func (client *MockK8sClient) CreateIngress(namespace string, ingress *networkv1.Ingress) (*networkv1.Ingress, error) {
+	return client.ingress, client.err
+}
+
+func (client *MockK8sClient) CreateConfigMap(namespace string, configMap *corev1.ConfigMap) (*corev1.ConfigMap, error) {
+	return client.configmap, client.err
+}
+
+func (client *MockK8sClient) GetConfigMap(namespace string, name string) (*corev1.ConfigMap, error) {
+	return client.configmap, client.err
+}
+
+func (client *MockK8sClient) UpdateConfigMap(namespace string, configMap *corev1.ConfigMap) (*corev1.ConfigMap, error) {
+	return client.configmap, client.err
+}
+
+func (client *MockK8sClient) CreateDeploymentWithName(namespace string, deployment *appsv1.Deployment, name string) (*appsv1.Deployment, error) {
+	return client.deployment, client.err
+}
+
+func (client *MockK8sClient) CreateDeployment(namespace string, deployment *appsv1.Deployment) (*appsv1.Deployment, error) {
+	return client.deployment, client.err
+}
+
+func (client *MockK8sClient) DeleteDeployment(namespace string, name string) error {
+	return client.err
+}
+
+func (client *MockK8sClient) CreateNamespace(name string) (*corev1.Namespace, error) {
+	return client.namespace, client.err
+}
+
+func (client *MockK8sClient) GetNamespace(name string) (*corev1.Namespace, error) {
+	return client.namespace, client.err
+}
+
+func (client *MockK8sClient) GetFileFromPod(namespace string, pod *corev1.Pod, path string) (string, error) {
+	return client.fileContent, client.err
+}
+
+func (client *MockK8sClient) CreateConfigmapWithConfig(namespace string, template string, items map[string]string) error {
+	return client.err
+}
+
+func (client *MockK8sClient) CreateNamespaceForApp(name string) {
+
+}
+
+func (client *MockK8sClient) GetPodsOfDeployment(namespace string, deployment string) (*corev1.PodList, error) {
+	return client.podList, client.err
+}
+
+func (client *MockK8sClient) WaitingDeploymentCreated(namespace string, name string) error {
+	return client.err
+}
+
+func (client *MockK8sClient) Exec(namespace string, pod *corev1.Pod, command []string) ([]byte, []byte, error) {
+	return client.stdout, client.stderr, client.err
 }
 
 func TestCreateDeployer(t *testing.T) {
--- pkg/kubernetes/k8s.go
+++ pkg/kubernetes/k8s.go
@@ -1,6 +1,9 @@
 package kubernetes
 
 import (
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	networkv1 "k8s.io/api/networking/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/kubernetes/fake"
 	"k8s.io/client-go/rest"
@@ -11,6 +14,29 @@ type Config struct {
 	InCluster      bool
 	KubeconfigPath string
 	ManifestPath   string
+}
+
+type IK8s interface {
+	GetPodStatus(namespace, name string) (string, error)
+	CreateStatefulSet(namespace string, statefulSet *appsv1.StatefulSet) (*appsv1.StatefulSet, error)
+	CreateService(namespace string, service *corev1.Service) (*corev1.Service, error)
+	CreatePersistentVolumeClaim(namespace string, pvc *corev1.PersistentVolumeClaim) (*corev1.PersistentVolumeClaim, error)
+	CreateConfigMapWithConfig(namespace string, configMap *corev1.ConfigMap, config map[string]string) (*corev1.ConfigMap, error)
+	CreateIngress(namespace string, ingress *networkv1.Ingress) (*networkv1.Ingress, error)
+	CreateConfigMap(namespace string, configMap *corev1.ConfigMap) (*corev1.ConfigMap, error)
+	GetConfigMap(namespace string, name string) (*corev1.ConfigMap, error)
+	UpdateConfigMap(namespace string, configMap *corev1.ConfigMap) (*corev1.ConfigMap, error)
+	CreateDeploymentWithName(namespace string, deployment *appsv1.Deployment, name string) (*appsv1.Deployment, error)
+	CreateDeployment(namespace string, deployment *appsv1.Deployment) (*appsv1.Deployment, error)
+	DeleteDeployment(namespace string, name string) error
+	CreateNamespace(name string) (*corev1.Namespace, error)
+	GetNamespace(name string) (*corev1.Namespace, error)
+	GetFileFromPod(namespace string, pod *corev1.Pod, path string) (string, error)
+	CreateConfigmapWithConfig(namespace string, template string, items map[string]string) error
+	CreateNamespaceForApp(name string)
+	GetPodsOfDeployment(namespace string, deployment string) (*corev1.PodList, error)
+	WaitingDeploymentCreated(namespace string, name string) error
+	Exec(namespace string, pod *corev1.Pod, command []string) ([]byte, []byte, error)
 }
 
 type Kubernetes struct {
