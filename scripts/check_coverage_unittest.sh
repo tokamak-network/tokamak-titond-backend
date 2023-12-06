0fcd52b37ef25540f6a381e9ac4caf1b4cc83f67
nguyenzung <nguyenvietdungcs52@gmail.com>
2023-12-06 09:35:09 +0700

    OR-1103 update github action

--- scripts/check_coverage_unittest.sh
+++ scripts/check_coverage_unittest.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 expected=70
-actual=$(go tool cover -func=coverage.txt | grep ^total | awk '{print $3}' | cut -d. -f1)
+actual=$(go tool cover -func=coverage.out | grep ^total | awk '{print $3}' | cut -d. -f1)
 echo "Actual coverage is $actual%"
 if [ "$actual" -lt "$expected" ]; then
   echo "Error: The coverage is $actual%. The threshold is $expected%."
